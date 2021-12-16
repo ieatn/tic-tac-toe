c76a67aa25e4c2f19c4df7c000eb05201784b1f5
Dennis Zhang <81874063+ieatn@users.noreply.github.com>
2021-12-15 17:39:31 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-
+# board
+board = ['-', '-', '-',
+         '-', '-', '-',
+         '-', '-', '-'] 
+# display board
+def display_board():
+    print(board[0]+' | '+board[1]+' | '+board[2])
+    print(board[3]+' | '+board[4]+' | '+board[5])
+    print(board[6]+' | '+board[7]+' | '+board[8])
+# play game
+# handle turn
+# check win
+    # check rows
+    # check columns
+    # check diagonals
+# check tie
+# flip player x to o
+display_board()
